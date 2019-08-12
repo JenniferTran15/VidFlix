f4d226339929755af2061c4433545208970dc766
Jennifer <jennifertran135@gmail.com>
2019-08-12 21:08:39 +0000

    trying to push to heroku

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -16,6 +16,9 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
+    @section = Section.new
+    @lesson = Lesson.new
+    @course = Course.find(params[:id])
   end
 
   private
