e3ebbb08728fc0b1e27b86cef56a1322eaacc299
Jennifer <jennifertran135@gmail.com>
2019-08-15 03:54:36 +0000

    Lesson Drag & Drop

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
@@ -7,4 +10,17 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  helper_method :require_authorized_for_current_course
+  def current_course
+    current_lesson.section.current_course
+  end
+
+  def require_authorized_for_current_course
+    if ! current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Must Be Enrolled to Access t his Course'
+    end
+  end
 end
+
