3bbe560be97e20a0eb18516c63e54c93dc21dce1
Jennifer <jennifertran135@gmail.com>
2019-10-24 16:30:59 +0000

    heroku push

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Flixter
 
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
 
   end
 end
