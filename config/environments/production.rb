6363a6bae859ef10cf973ba6e9bb2329da700c7f
Jennifer <jennifertran135@gmail.com>
2019-10-29 16:44:48 +0000

    fixing heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = {host: 'https://vidflix.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'vidflix.herokuapp.com'}
+  config.assets.js_compressor = Uglifier.new(:harmony => true)
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
